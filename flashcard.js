29a9b4f0d7c29ba99513241ba201e4f7d1e985e6
isumizumi <isumi.karina72@gmail.com>
2017-02-10 17:17:31 +0700

    Progress

--- flashcard.js
+++ flashcard.js
@@ -1,1 +1,7 @@
+"use strict"
 
+import Controller from "./controller"
+let input        = process.argv[2]
+// console.log('tes');
+const controller = new Controller(input)
+controller.play()
--- model.js
+++ model.js
@@ -1,1 +1,15 @@
+"use strict"
 
+import fs from "fs"
+
+export default class Model {
+  constructor(data) {
+    this.data   = data
+  }
+
+  getData() {
+    return JSON.parse(fs.readFileSync(this.data, "utf-8"))
+  }
+}
+
+// module.exports = Model
