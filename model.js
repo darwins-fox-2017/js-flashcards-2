2453114a53699a9aeb9811a116a3eb0e0190b4a2
Eri Irawan <eryirawan91@gmail.com>
2017-02-10 16:10:57 +0700

    Initial commit

--- model.js
+++ model.js
@@ -1,1 +1,13 @@
+"use strict"
 
+import fs from 'fs'
+
+export default class Model {
+  constructor(file) {
+    this.file = file
+  }
+
+  getData(){
+    return JSON.parse(fs.readFileSync(this.file, "utf-8"))
+  }
+}
