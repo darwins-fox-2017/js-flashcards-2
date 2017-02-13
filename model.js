fc3dc7f48c0096293ee28dc0c059f16ef2016e45
pisanggoreng <7ofpentacles@gmail.com>
2017-02-13 14:21:37 +0700

    clear

--- model.js
+++ model.js
@@ -1,1 +1,16 @@
+const fs = require('fs')
 
+class Model {
+  constructor(path) {
+    this.path = path
+    this.data = JSON.parse(fs.readFileSync(this.path,'utf-8'))
+  }
+
+  question() {
+    return this.data
+  }
+}
+
+// export class
+// tulis jangan pakai 's'
+export default Model
--- view.js
+++ view.js
@@ -1,1 +1,21 @@
 
+
+class View {
+  static welcome_message() {
+    return 'hallo selamat datang';
+  }
+
+  static correct_message() {
+    return "Jawaban anda benar."
+  }
+
+  static finish_message() {
+    return "Selamat, jawaban anda betul semua, silahkan ambil hadiah dari Enci"
+  }
+
+  static wrong_answer() {
+    return 'Jawaban anda salah, silahkan dicoba kembali ya'
+  }
+}
+
+export default View;
